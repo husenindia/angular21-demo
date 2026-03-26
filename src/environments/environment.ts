76c79511a1b5e015b6728c7c813fdbd352fdfaa4
husen.telwala <husen.telwala@autofacets.in>
2026-03-26 23:30:53 +0530

    Enviroment URL change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   //  apiUrl: 'https://localhost:7283/api' // local
-  apiUrl: 'https://producttest-fwc6a6gedfhscegb.centralindia-01.azurewebsites.net' // production
+  apiUrl: 'https://productbackend-gbb2ddaeg5cndqh9.centralindia-01.azurewebsites.net' // production
 };
